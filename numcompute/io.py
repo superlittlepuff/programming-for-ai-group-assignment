1789a670309a83d5b9bfc501277601d6e5279118
superlittlepuff <2271209136@qq.com>
2026-04-15 22:56:08 +0930

    Initial commit

---
+++ numcompute/io.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+def load_csv(file_name = "student_performance_data.csv", delimiter = ",", skip= 1):
+
+    try:
+        arr = np.genfromtxt(file_name,
+                        delimiter= delimiter,
+                        skip_header= skip,
+                        filling_values= np.nan,
+                        encoding = "utf-8",
+                        dtype = str)
+        arr[arr == ''] = np.nan
+    except FileNotFoundError:
+        raise FileNotFoundError("File not found")
+
+    if arr.size == 0:
+        raise ValueError("Empty csv file")
+    return arr
+
+
+# a = load_csv()
+# print(a)
+# print(type(a))
+# print(a.shape)
---
+++ numcompute/preprocessing.py
@@ -0,0 +1,162 @@
+import numpy as np
+
+class StandardScaler:
+    def __init__(self):
+        self.mean_ = None
+        self.scale_ = None
+        self.n_features_in_ = None
+        self._is_fitted = False
+
+    def fit(self, X):
+        X = np.asarray(X, dtype=float)
+
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+
+        self.n_features_in_ = X.shape[1]
+
+        self.mean_ = np.nanmean(X, axis=0)
+        self.scale_ = np.nanstd(X, axis=0)
+
+        self._is_fitted = True
+        return self
+
+    def transform(self, X):
+        if not self._is_fitted:
+            raise ValueError("StandardScaler has not been fitted yet.")
+
+        X = np.asarray(X, dtype=float)
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError("Input feature count does not match fitted data.")
+
+        X_out = (X - self.mean_) / self.scale_
+        self.scale_[self.scale_ == 0] = 1.0
+
+        return X_out
+
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
+
+class MinMaxScaler:
+    def __init__(self):
+        self.max_ = None
+        self.min_ = None
+        self.range_ = None
+        self.n_features_in_ = None
+        self._is_fitted = False
+
+    def fit(self, X):
+        X = np.asarray(X, dtype=float)
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+
+        self.n_features_in_ = X.shape[1]
+
+        self.min_ = np.nanmin(X, axis=0)
+        self.max_ = np.nanmax(X, axis=0)
+        self.range_ = self.max_ - self.min_
+        self.range_[self.range_ == 0] = 1.0
+
+        self._is_fitted = True
+        return self
+
+    def transform(self, X):
+        if not self._is_fitted:
+            raise ValueError("MinMaxScaler has not been fitted yet.")
+
+        X = np.asarray(X, dtype=float)
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError("Input feature count does not match fitted data.")
+
+        X_out = (X - self.min_) / self.range_
+
+        return X_out
+
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
+
+class OneHotEncoder:
+    def __init__(self):
+        self.categories_ = None
+        self.n_features_in_ = None
+        self._is_fitted = False
+
+    def fit(self, X):
+        X = np.asarray(X, dtype=object)
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+
+        self.n_features_in_ = X.shape[1]
+        self.categories_ = []
+
+        for i in range(self.n_features_in_):
+            col = X[:, i]
+            cats = np.unique(col)
+            self.categories_.append(cats)
+
+        self._is_fitted = True
+        return self
+
+    def transform(self, X):
+        if not self._is_fitted:
+            raise ValueError("OneHotEncoder has not been fitted yet.")
+
+        X = np.asarray(X, dtype=object)
+
+        if X.ndim != 2:
+            raise ValueError("The dimension of X should be 2")
+        if X.size == 0:
+            raise ValueError("The input array is empty")
+        if X.shape[1] != self.n_features_in_:
+            raise ValueError("Input feature count does not match fitted data.")
+
+        encoded_columns = []
+
+        for i in range(self.n_features_in_):
+            col = X[:, i]
+            cats = self.categories_[i]
+            one_hot = np.zeros((len(col), len(cats)))
+
+            for row_idx in range(len(col)):
+                value = col[row_idx]
+
+                if value not in cats:
+                    raise ValueError(f"Unknown category '{value}' in column {i}")
+
+                cat_idx = np.where(cats == value)[0][0]
+                one_hot[row_idx, cat_idx] = 1
+
+            encoded_columns.append(one_hot)
+
+        X_out = np.concatenate(encoded_columns, axis=1)
+        return X_out
+
+    def fit_transform(self, X):
+        return self.fit(X).transform(X)
+
+class SimpleImputer:
+    ...
